9853944b4a4a64e6c5f85fdb4dae44b27fffdeb3
mesli <mohamed.mesli@etu.univ-amu.fr>
2026-03-15 22:07:16 +0100

    feat: add application routing with react-router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,38 +1,40 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import AuthChoice from "./pages/AuthChoice";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
-import Objectifs from "./pages/Objectifs"
-import Flow from "./pages/Flow"
-import Profil from "./pages/Profil"
-import Communaute from "./pages/Communaute"
-import Parametres from "./pages/Parametres"
+import Objectifs from "./pages/Objectifs";
+import Flow from "./pages/Flow";
+import Profil from "./pages/Profil";
+import Communaute from "./pages/Communaute";
+import Parametres from "./pages/Parametres";
 
 function App() {
 
-  return (
+    return (
 
-      <Routes>
+        <BrowserRouter>
 
-        <Route path="/" element={<AuthChoice />} />
+            <Routes>
 
-        <Route path="/login" element={<Login />} />
+                <Route path="/" element={<AuthChoice />} />
 
-        <Route path="/signup" element={<Signup />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
 
-        <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/objectifs" element={<Objectifs/>} />
-          <Route path="/flow" element={<Flow/>} />
-          <Route path="/profil" element={<Profil/>} />
-          <Route path="/communaute" element={<Communaute/>} />
-          <Route path="/parametres" element={<Parametres/>} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/objectifs" element={<Objectifs />} />
+                <Route path="/flow" element={<Flow />} />
+                <Route path="/profil" element={<Profil />} />
+                <Route path="/communaute" element={<Communaute />} />
+                <Route path="/parametres" element={<Parametres />} />
 
+            </Routes>
 
-      </Routes>
+        </BrowserRouter>
 
-  );
+    );
 
 }
 
